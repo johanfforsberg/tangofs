6e1b57facdcc70ce0de163e2e0c3be02431faaff
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2015-08-08 23:43:25 +0200

    plugin fix

--- tangofs/plugins/__init__.py
+++ tangofs/plugins/__init__.py
@@ -1,21 +1,27 @@
+"""
+A hacky plugin system for file formatters
+"""
+
 import imp
 import os
 import glob
 
+
 plugins = []
 
+# register all modules in this directory, not very elegant
 for f in glob.glob(os.path.dirname(__file__)+"/*.py"):
-    if f.endswith("__init__.py"):
-        continue
     name = os.path.splitext(os.path.basename(f))[0]
-    plugins.append(imp.load_source(name, f))
+    if name != "__init__":
+        plugin = imp.load_source(name, f)
+        plugins.append(plugin)
 
 
 def get_plugins(info):
-    "Find any suitable plugins for the given attribute"
+    "Find any suitable plugins given an attribute info object"
     matches = []
     for plugin in plugins:
-        print "testing", plugin.spec["name"],
+        print "Testing", plugin.spec["name"],
         data_type = plugin.spec.get("data_type")
         data_format = plugin.spec.get("data_format")
         if all((data_type is None or data_type == info.data_type,
